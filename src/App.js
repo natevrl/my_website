feac625984fde04b07189dfde2feefec1212442b
v3r <v3rslor@protonmail.com>
2022-03-03 01:41:06 +0100

    init pages

--- src/App.js
+++ src/App.js
@@ -5,20 +5,43 @@ import Home from './pages/Home';
 import Creations from './pages/Creations';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import Not_found from './pages/Not_found';
+import Navbar from './components/Navbar';
+// import Not_found from './pages/Not_found';
+
+// var Container = React.createClass({
+// 	render: function () {
+// 		return <div>
+// 			<Home />
+// 			<Contact />
+// 			<Creations />
+// 			<About />
+// 		</div>;
+// 	}
+// });
 
 const App = () => {
+
 	return (
-		<>
-			<Routes>
-				<Route path="/" exact element={<Home/>} />
-				<Route path="/about" element={<About/>} />
-				<Route path="/creations" element={<Creations/>} />
-				<Route path="/contact" element={<Contact/>} />
-				<Route path="/error" element={<Not_found/>} />
-			</Routes>
-		</>
+		<div className="App">
+			<div id="content">
+				<Routes>
+					<Route path="/" element={<Home/>}/>
+					<Route path="/creations" element={<Creations/>} />
+					<Route path="/about" element={<About/>} />
+					<Route path="/contact" element={<Contact/>} />
+				</Routes>
+				<Navbar />
+			</div>
+		</div>
 	);
 };
+
+{/* <Routes>
+	<Route path="/" exact element={<Home/>} />
+	<Route path="/about" element={<About/>} />
+	<Route path="/creations" element={<Creations/>} />
+	<Route path="/contact" element={<Contact/>} />
+	<Route path="/error" element={<Not_found/>} />
+</Routes> */}
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,59 +4,53 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
 	return (
 		<div className='sidebar'>
-			<div className='id'>
-				<div className='idContent'>
-					<img src="./media/photo-intra.jpg" alt="profil-pic" />
-					<h3>nbenhado</h3>
-				</div>
-			</div>
-
 			<div className='navigation'>
-				<ul>
+				<NavLink exact to="/" activeClassName="navcAtive">
+					{/* importe licone de fontawesome */}
+					<i className='fas fa-home'>
+						<span> /home</span>
+					</i>
+				</NavLink>
 
-					<li>
-						<NavLink exact to="/" activeClassName="navAtive">
-							{/* importe licone de fontawesome */}
-							<i className='fas fa-home'>
-								<span>home</span>
-							</i>
-						</NavLink>
-					</li>
-	
-					<li>
-						<NavLink exact to="/about" activeClassName="navAtive">
-							{/* importe licone de fontawesome */}
-							<i className='fas fa-mountain'>
-								<span>about me</span>
-							</i>
-						</NavLink>
-					</li>
 
-					<li>
-						<NavLink exact to="/creations" activeClassName="navAtive">
-							{/* importe licone de fontawesome */}
-							<i className='fas fa-images'>
-								<span>creations</span>
-							</i>
-						</NavLink>
-					</li>
 
-					<li>
-						<NavLink exact to="/contact" activeClassName="navAtive">
-							{/* importe licone de fontawesome */}
-							<i className='fas fa-address-book'>
-								<span>contact</span>
-							</i>
-						</NavLink>
-					</li>
-				</ul>
+				<NavLink
+					to="/about"
+					activeStyle={{
+						fontWeight: "bold",
+						color: "red"
+					}}
+					>
+					FAQs
+					</NavLink>
+
+
+				{/* <NavLink exact to="/about" activeClassName="navActive">
+					<i className='fas fa-mountain'>
+						<span> /about me</span>
+					</i>
+				</NavLink> */}
+
+				<NavLink exact to="/creations" activeClassName="navActive">
+					{/* importe licone de fontawesome */}
+					<i className='fas fa-images'>
+						<span> /creations</span>
+					</i>
+				</NavLink>
+
+				<NavLink exact to="/contact" activeClassName="navActive">
+					{/* importe licone de fontawesome */}
+					<i className='fas fa-address-book'>
+						<span> /contact</span>
+					</i>
+				</NavLink>
 			</div>
 
-			<div className='socialNetwork'>
+			{/* <div className='socialNetwork'>
 					<a href="https://github.com/N4t4nis"><i className='fas fa-github'></i></a>
 					<a href="https://www.linkedin.com/"><i className='fas fa-linkedin'></i></a>
 					<a href="https://tryhackme.com/"><i className="fas fa-robot"></i></a>
-			</div>
+			</div> */}
 
 		</div>
 	);
