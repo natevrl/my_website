c9bac05fe53540f99088d56931cc9096ed346807
n4t <n4t4nistorus@gmail.com>
2022-10-25 20:54:50 -0400

    writing card project

--- src/data/projectsDb.js
+++ src/data/projectsDb.js
@@ -3,43 +3,43 @@ import { v4 as uuidv4 } from 'uuid';
 const ProjectsDb = [{
 	"id": uuidv4(),
 	"title": "Minishell",
-	"description": "Recreate the fonctionement of bash shell by executing command, handling signals, redir...",
-	"link": "https://github.com/nbenhado",
+	"description": "Reproduction du fonctionnement d'un shell bash, exécution de commandes, gestions des signaux, redirections, multiples pipes...",
+	"link": "https://github.com/natevrl/minishell",
 	"tags": ["All", "C", "Unix/Bash"],
 	"stack": ["linked lists", "unix signals", "execve"]
 },
 {
 	"id": uuidv4(),
 	"title": "Cub3d",
-	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"description": "Développement d'un FPS 3D en C inspiré du célèbre Wolfenstein 1992, résolue grace à la technique du ray-casting",
+	"link": "https://github.com/natevrl/cub3d",
 	"tags": ["All", "C"],
 	"stack": ["linked lists", "raycasting algorithm"]
 
 },
 {
 	"id": uuidv4(),
-	"title": "AxialMovies",
-	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"title": "Stier Movies",
+	"description": "Recherchez et ajoutez vos films préférés en favoris. Coder en React.js, feature : fetching API, inifinite scroll, redux et local storage",
+	"link": "https://github.com/natevrl/StierMovies",
 	"tags": ["All", "Web"],
-	"stack": ["sass", "redux", "mui"]
+	"stack": ["react", "redux", "API", "router v6", "sass", "mui"]
 
 },
 {
 	"id": uuidv4(),
-	"title": "OwnPortefolio",
-	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"title": "nathanvrl.tech",
+	"description": "Première version de ce site codé en React.js, avec des features comme un système de filtrage, back-end, des animations, du responsive design...",
+	"link": "https://github.com/natevrl/natevrl.github.io",
 	"tags": ["All", "Web"],
-	"stack": ["linked lists", "unix signals", "execve"]
+	"stack": ["react", "router v6", "sass", "mui", "gsap", "back-end"]
 
 },
 {
 	"id": uuidv4(),
 	"title": "Minitalk",
 	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"link": "https://github.com/natevrl/minishell",
 	"tags": ["All", "C", "Unix/Bash"],
 	"stack": ["linked lists", "unix signals", "execve"]
 
@@ -48,7 +48,7 @@ const ProjectsDb = [{
 	"id": uuidv4(),
 	"title": "so_long",
 	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"link": "https://github.com/natevrl/minishell",
 	"tags": ["All", "C"],
 	"stack": ["linked lists", "unix signals", "execve"]
 
@@ -57,7 +57,7 @@ const ProjectsDb = [{
 	"id": uuidv4(),
 	"title": "push_swap",
 	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"link": "https://github.com/natevrl/minishell",
 	"tags": ["All", "C"],
 	"stack": ["linked lists", "unix signals", "execve"]
 
@@ -66,7 +66,7 @@ const ProjectsDb = [{
 	"id": uuidv4(),
 	"title": "get_next_line",
 	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"link": "https://github.com/natevrl/minishell",
 	"tags": ["All", "C"],
 	"stack": ["linked lists", "unix signals", "execve"]
 
@@ -75,7 +75,7 @@ const ProjectsDb = [{
 	"id": uuidv4(),
 	"title": "printf",
 	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"link": "https://github.com/natevrl/minishell",
 	"tags": ["All", "C"],
 	"stack": ["linked lists", "unix signals", "execve"]
 
@@ -84,7 +84,7 @@ const ProjectsDb = [{
 	"id": uuidv4(),
 	"title": "philosophers",
 	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"link": "https://github.com/natevrl/minishell",
 	"tags": ["All", "C"],
 	"stack": ["linked lists", "unix signals", "execve"]
 
@@ -93,7 +93,7 @@ const ProjectsDb = [{
 	"id": uuidv4(),
 	"title": "Piscine_42",
 	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"link": "https://github.com/natevrl/minishell",
 	"tags": ["All", "C", "Unix/Bash"],
 	"stack": ["linked lists", "unix signals", "execve"]
 
@@ -102,7 +102,7 @@ const ProjectsDb = [{
 	"id": uuidv4(),
 	"title": "libft",
 	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"link": "https://github.com/natevrl/minishell",
 	"tags": ["All", "C"],
 	"stack": ["linked lists", "unix signals", "execve"]
 
@@ -111,7 +111,7 @@ const ProjectsDb = [{
 	"id": uuidv4(),
 	"title": "blacksmith_",
 	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"link": "https://github.com/natevrl/minishell",
 	"tags": ["All", "Web"],
 	"stack": ["linked lists", "unix signals", "execve"]
 
@@ -120,7 +120,7 @@ const ProjectsDb = [{
 	"id": uuidv4(),
 	"title": "Eagl",
 	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"link": "https://github.com/natevrl/minishell",
 	"tags": ["All", "Web"],
 	"stack": ["linked lists", "unix signals", "execve"]
 
@@ -129,7 +129,7 @@ const ProjectsDb = [{
 	"id": uuidv4(),
 	"title": "roadmap.sh",
 	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"link": "https://github.com/natevrl/minishell",
 	"tags": ["All", "Web"],
 	"stack": ["linked lists", "unix signals", "execve"]
 
@@ -138,7 +138,7 @@ const ProjectsDb = [{
 	"id": uuidv4(),
 	"title": "UberClone",
 	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"link": "https://github.com/natevrl/minishell",
 	"tags": ["All", "Web"],
 	"stack": ["linked lists", "unix signals", "execve"]
 
@@ -147,7 +147,7 @@ const ProjectsDb = [{
 	"id": uuidv4(),
 	"title": "script_workplaces",
 	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"link": "https://github.com/natevrl/minishell",
 	"tags": ["All", "Unix/Bash"],
 	"stack": ["linked lists", "unix signals", "execve"]
 
@@ -156,7 +156,7 @@ const ProjectsDb = [{
 	"id": uuidv4(),
 	"title": "born2beroot",
 	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"link": "https://github.com/natevrl/minishell",
 	"tags": ["All", "Unix/Bash"],
 	"stack": ["linked lists", "unix signals", "execve"]
 
@@ -165,7 +165,7 @@ const ProjectsDb = [{
 	"id": uuidv4(),
 	"title": ".vimrc",
 	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"link": "https://github.com/natevrl/minishell",
 	"tags": ["All", "Unix/Bash"],
 	"stack": ["linked lists", "unix signals", "execve"]
 
@@ -174,7 +174,7 @@ const ProjectsDb = [{
 	"id": uuidv4(),
 	"title": "w3ather",
 	"description": "coming soon...",
-	"link": "https://github.com/nbenhado",
+	"link": "https://github.com/natevrl/minishell",
 	"tags": ["All", "Web"],
 	"stack": ["linked lists", "unix signals", "execve"]
 }];
